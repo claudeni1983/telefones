a7291ba128289b4ed96aec3b21fb4e3d0482b509
claudeni1983 <67796600+claudeni1983@users.noreply.github.com>
2025-07-06 23:00:01 -0300

    Actions

--- srv/service.cds
+++ srv/service.cds
@@ -15,6 +15,20 @@ service CatalogService {
     ])   
     as projection on my.Contatos;
 
+    type userNome : {
+        nome : String
+    }
+
+    type tContato : {
+        nome : String;
+        fone : String;
+        dataNasc : Date; 
+    }
+
+    action getByName (userName : userNome) returns String;
+
+    action InsertContato (contato : tContato) returns contatos;
+
 //    action processContacts (input: String) returns String;
 
 }
--- srv/service.js
+++ srv/service.js
@@ -2,8 +2,25 @@ const cds = require('@sap/cds');
 
 module.exports = cds.service.impl(async function(){
 
-    this.on('processContacts', async (req) => {
-        return "My test...";
+    this.on('getByName', async (req) => {
+        const nome = req.data.userName.nome;
+        const cont = await SELECT.from('telefones.Contatos').columns('fone');
+        return JSON.stringify(cont); //`My name is ${nome}`;
+    });
+
+    this.on('InsertContato', async (req) => {
+
+        const inputContato = req.data.contato;        
+        const outputContato = [
+            {
+                nome : inputContato.nome,
+                fone : inputContato.fone,
+                dataNasc : inputContato.dataNasc 
+            }
+        ];
+
+        await INSERT (outputContato) .into ('telefones.Contatos');
+
     });
 
 });
